b3004fcc980f81fba43bfba81e9b58fef22131ea
Aleksey Kravetz <alex@alekseys-mbp.mynet>
2019-10-29 15:18:12 +0200

    port 3000

--- src/server.js
+++ src/server.js
@@ -10,6 +10,6 @@ app.use(cors());
 
 app.use('/', express.static('dist'));
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`dx server - port: ${port} - ${process.env.NODE_ENV || 'local'} environment`));
